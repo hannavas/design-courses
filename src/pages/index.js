4c1ca1203c08749dcbba75885a9d454521dbf7bf
Savannah <ssl399@nyu.edu>
2018-12-10 01:38:31 -0500

    add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Design School</h1>
-    <p>Peruse resources from design courses at colleges around the world.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+    <div>
+        <div className="Hero">
+            <div className="HeroGroup">
+                <h1>Design School</h1>
+                <p>Peruse resources from design courses at colleges around the world.</p>
+                <Link to="/video">Watch the video</Link>
+            </div>
+        </div>
+    </div>
 )
 
 export default IndexPage
