76964e5f29a2cb1dff59c32fc821b932efe31f3a
Savannah <ssl399@nyu.edu>
2018-12-10 00:12:51 -0500

    commit for kicks

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Savannah\'s Other Website,
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Design School</h1>
+    <p>Peruse resources from design courses at colleges around the world.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
