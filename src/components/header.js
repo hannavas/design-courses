defe495b0a3da73814e26c04ff2bd946e84dd362
Savannah <ssl399@nyu.edu>
2018-12-12 09:10:19 -0500

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,9 @@ const Header = ({ siteTitle }) => (
     <div className="Header">
         <div className="HeaderGroup">
             {/* <Link to="/"><img width="30" /></Link>*/}
-            <Link to="/courses">Courses</Link>
-            <Link to="/readings">Readings</Link>
-            <Link to="/videos">Videos</Link>
+            <Link to="/courses">Library</Link>
+            <Link to="/readings">Index</Link>
+            <Link to="/videos">Newsletter</Link>
             <Link to="/about">About</Link>
             <Link to="/donate"><button>Donate</button></Link>
         </div>
