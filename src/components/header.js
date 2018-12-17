63474c97865c970c01bff58a2ef9b9c210b5a03d
Savannah <ssl399@nyu.edu>
2018-12-16 21:46:44 -0500

    add scroll interaction

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-// TODO: include image
-const Header = ({ siteTitle }) => (
-    <div className="Header">
-        <div className="HeaderGroup">
-            {/* <Link to="/"><img width="30" /></Link>*/}
-            <Link to="/courses">Library</Link>
-            <Link to="/readings">Index</Link>
-            <Link to="/videos">Newsletter</Link>
-            <Link to="/about">About</Link>
-            <Link to="/donate"><button>Donate</button></Link>
-        </div>
-    </div>
-)
+class Header extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            hasScrolled: false
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = (event) => {
+        const scrollTop = window.pageYOffset
+        if (scrollTop > 50) {
+            this.setState({ hasScrolled: true })
+        } else {
+            this.setState({ hasScrolled: false })
+        }
+    }
+
+    render() {
+        return (
+            <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+                <div className="HeaderGroup">
+                    {/* <Link to="/"><img width="30" /></Link>*/}
+                    <Link to="/courses">Library</Link>
+                    <Link to="/readings">Index</Link>
+                    <Link to="/videos">Newsletter</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/donate"><button>Donate</button></Link>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Header
