e36d67255a86afee0e25cef8cffae68f271bc19d
Savannah <ssl399@nyu.edu>
2018-12-19 23:21:37 -0500

    update config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: 'Design School Syllabus'
+        title: 'Design School',
+        description: 'Peruse syllabi from design courses at colleges around the world.',
+        keywords: 'design, school, syllabus, class, course, curriculum'
     },
     plugins: ['gatsby-plugin-react-helmet']
 }
